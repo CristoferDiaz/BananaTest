dd81ec790003931f39c739fcee7c8d7f6c8520ef
César Ravelo Martínez <crmprograming@gmail.com>
2021-01-16 09:52:06 +0000

    Creada estructura básica del proyecto

---
+++ src/main/java/dad/proyectos/banana_test/App.java
@@ -0,0 +1,25 @@
+package dad.proyectos.banana_test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App  extends Application {
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO: Especificar controlador principal
+		/*
+		controller = new MainController();
+		Scene escena = new Scene(controller.getView());
+
+		primaryStage.setScene(escena);
+		primaryStage.setTitle("Ventana");
+		primaryStage.show();
+		*/
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/dad/proyectos/banana_test/Main.java
@@ -0,0 +1,9 @@
+package dad.proyectos.banana_test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		App.main(args);
+	}
+
+}
