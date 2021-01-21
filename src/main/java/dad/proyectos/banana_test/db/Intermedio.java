d2e1c8db1a5160c2fea57450bda24497f886450d
SamirElKharrat <samirelkharrat@gmail.com>
2021-01-21 17:32:52 +0000

    Creamos visualizar preguntas y examenes
    En esta funciones hacemos unos select para poder hacer la visualizacion de las preguntas y las respuestas

--- src/main/java/dad/proyectos/banana_test/db/Intermedio.java
+++ src/main/java/dad/proyectos/banana_test/db/Intermedio.java
@@ -35,6 +35,56 @@ public abstract class Intermedio {
 		}
 	}
 
+	// Funcion con la que poder visualizar todas las preguntas de la tabla
+	// bt_preguntas
+	public boolean visualizarPreguntas() {
+		Connection con = conectarmysql();
+		boolean resultado = false;
+		int id;
+		String tipoPregunta, contenido;
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT id, tipoPregunta, contenido FROM bt_preguntas");
 
+			while (rs.next()) {
+
+				id = rs.getInt("id");
+				tipoPregunta = rs.getString("tipoPregunta");
+				contenido = rs.getString("contenido");
+			}
+
+			resultado = true;
+			rs.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+	}
+
+	// Funcion con la que poder visualizar todos los examenes de la tabla
+	// bt_examenes
+	public void visualizarExamenes() {
+		Connection con = conectarmysql();
+		int id;
+		String nombre, descGeneral;
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT id, nombre, descripcionGeneral FROM bt_examenes");
+
+			while (rs.next()) {
+
+				id = rs.getInt("id");
+				nombre = rs.getString("nombre");
+				descGeneral = rs.getString("descripcionGeneral");
+			}
+
+			rs.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
