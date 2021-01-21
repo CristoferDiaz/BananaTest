61e8ca6bc9e29f83cc978ad6bac0d52cd0f99837
SamirElKharrat <samirelkharrat@gmail.com>
2021-01-21 17:32:25 +0000

    Creamos la funcion de conexión y la de desconexión

--- src/main/java/dad/proyectos/banana_test/db/Intermedio.java
+++ src/main/java/dad/proyectos/banana_test/db/Intermedio.java
@@ -13,5 +13,28 @@ public abstract class Intermedio {
 	static Scanner sc = new Scanner(System.in);
 	public static String driver = "com.mysql.cj.jdbc.Driver";
 
+	// Funcion para conectarse a la base de datos que estamos usando
+	public static Connection conectarmysql() {
 
+		try {
+			Class.forName(driver);
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/bananatest", "root", "");
+		} catch (Exception e) {
+
+		}
+		return conexion;
+
+	}
+
+	// Funcion para desconectarse de la base de datos
+	void desconectarBD() {
+		try {
+			conexion.close();
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+
+	
 }
