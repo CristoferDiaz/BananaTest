efc744904a18eca76d34e1085ad0aa6c8297d772
César Ravelo Martínez <crmprograming@gmail.com>
2021-01-27 17:31:24 +0000

    Añadido efecto al icono del diálogo

--- src/main/java/dad/proyectos/banana_test/utils/DialogoConfirmar.java
+++ src/main/java/dad/proyectos/banana_test/utils/DialogoConfirmar.java
@@ -4,6 +4,7 @@ import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -20,7 +21,11 @@ public class DialogoConfirmar extends Dialogo<Boolean> {
 		Label lbContenido = new Label(contenido);
 		lbContenido.setStyle("-fx-font-weight:bold;");
 		Text warningIcon = GlyphsDude.createIcon(FontAwesomeIcon.WARNING, "120px");
+		DropShadow dropShadow = new DropShadow();
+		dropShadow.setOffsetX(3.0f);
+		dropShadow.setColor(Color.color(0.4f, 0.4f, 0.4f));
 		warningIcon.setFill(Color.rgb(232, 178, 36));
+		warningIcon.setEffect(dropShadow);
 		
 		root.getChildren().addAll(lbContenido, warningIcon);
 		
