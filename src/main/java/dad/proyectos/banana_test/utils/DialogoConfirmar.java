d61e7cbd58fca80774510495e4f0b2b10741bbbb
César Ravelo Martínez <crmprograming@gmail.com>
2021-01-27 09:57:50 +0000

    Actualizado diálogo para mostrar icono (falta color)

--- src/main/java/dad/proyectos/banana_test/utils/DialogoConfirmar.java
+++ src/main/java/dad/proyectos/banana_test/utils/DialogoConfirmar.java
@@ -1,7 +1,11 @@
 package dad.proyectos.banana_test.utils;
 
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class DialogoConfirmar extends Dialogo<Boolean> {
 
@@ -9,11 +13,16 @@ public class DialogoConfirmar extends Dialogo<Boolean> {
 		super(titulo, aceptar, cancelar);
 		
 		HBox root = new HBox();
+		root.setSpacing(5);
+		root.setAlignment(Pos.CENTER);
+		
 		Label lbContenido = new Label(contenido);
 		lbContenido.setStyle("-fx-font-weight:bold;");
+		// TODO: Cambiar color de icono
+		Text warningIcon = GlyphsDude.createIcon(FontAwesomeIcon.WARNING, "120px");
 		
-		root.getChildren().add(lbContenido);
-						
+		root.getChildren().addAll(lbContenido, warningIcon);
+		
 		getDialogPane().setContent(root);
 		
 		setResultConverter(dialogButton -> {
