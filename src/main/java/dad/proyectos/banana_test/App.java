b323487d7776b1a4ed698553f711ea9717dc596d
César Ravelo Martínez <crmprograming@gmail.com>
2021-01-30 17:03:45 +0000

    Implementada la gestión de preferencias

--- src/main/java/dad/proyectos/banana_test/App.java
+++ src/main/java/dad/proyectos/banana_test/App.java
@@ -1,11 +1,5 @@
 package dad.proyectos.banana_test;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import dad.proyectos.banana_test.controller.MainController;
 import dad.proyectos.banana_test.utils.Preferencias;
 import javafx.application.Application;
@@ -22,51 +16,26 @@ public class App extends Application {
 	
 	private MainController mainController;
 	
-	private void crearConfiguracionInicial(File file) throws IOException {
-		BufferedWriter bWriter = new BufferedWriter(new FileWriter(file));
-		String configuracionInicial = "";
-		
-		configuracionInicial += "idioma=es\n";
-		configuracionInicial += "tema=default\n";
-		
-		bWriter.write(configuracionInicial);
-		bWriter.close();
-	}
-	
 	@Override
 	public void init() throws Exception {
-		String rutaPerfil = System.getProperty("user.home");
-		File file = new File(rutaPerfil + "\\.BananaTest\\config.properties");
-		
-		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
-			throw new IllegalStateException("No se pudo crear la carpeta .BananaTest");
-		
-		if (file.createNewFile()) {
-			crearConfiguracionInicial(file);
-		}
-		
-		Preferencias.properties.load(new FileInputStream(file));
-	}
+		Preferencias.cargarPreferencias();
+	}	
 	
 	@Override
 	public void stop() throws Exception {
-		String rutaPerfil = System.getProperty("user.home");
-		File file = new File(rutaPerfil + "\\.BananaTest\\config.properties");
-	
-		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
-			throw new IllegalStateException("No se pudo crear la carpeta .VentanaConMemoria");
-		
-		// Creamos el fichero en caso de no existir
-		file.createNewFile();
-		
-		Preferencias.properties.store(new FileWriter(file), null);
-	}
+		Preferencias.guardarPreferencias();
+	}	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		mainController = new MainController();
 				
 		Scene escena = new Scene(mainController.getView());
+		
+		// Cargamos el tema css escogido por el usuario
+		if (Preferencias.getTema() != Preferencias.TEMAS.DEFAULT)
+			escena.getStylesheets().add(Preferencias.cargarTema());
+		
 		primaryStage.setScene(escena);
 		primaryStage.setTitle("BananaTest");
 		primaryStage.getIcons().add(new Image("/images/logo/bananatest_logo_16.png"));
--- src/main/java/dad/proyectos/banana_test/utils/Preferencias.java
+++ src/main/java/dad/proyectos/banana_test/utils/Preferencias.java
@@ -1,17 +1,103 @@
 package dad.proyectos.banana_test.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 
 public abstract class Preferencias {
 
-	public static Properties properties;
+	public static enum IDIOMAS {
+		ES,
+		EN
+	}
 	
+	public static enum TEMAS {
+		DEFAULT ("DEFAULT"),
+		DARK ("DARK");
+		
+		private final String name;
+		
+		private TEMAS(String string) {
+			this.name = string;
+		}
+		
+		@Override
+		public String toString() {
+			return this.name;
+		}
+		
+	}
+	
+	public static Properties properties;
+		
 	static {
 		properties = new Properties();
 	}
 	
-	public static String getTheme() {
-		return Preferencias.class.getResource(properties.getProperty("theme")).toExternalForm();
+	public static void cargarPreferencias() throws IOException, FileNotFoundException {
+		String rutaPerfil = System.getProperty("user.home");
+		File file = new File(rutaPerfil + "\\.BananaTest\\config.properties");
+		
+		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
+			throw new IllegalStateException("No se pudo crear la carpeta .BananaTest");
+		
+		if (file.createNewFile()) {
+			crearConfiguracionInicial(file);
+		}
+		
+		Preferencias.properties.load(new FileInputStream(file));
+	}
+	
+	private static void crearConfiguracionInicial(File file) throws IOException {
+		BufferedWriter bWriter = new BufferedWriter(new FileWriter(file));
+		String configuracionInicial = "";
+		
+		configuracionInicial += "idioma=es\n";
+		configuracionInicial += "tema=default\n";
+		
+		bWriter.write(configuracionInicial);
+		bWriter.close();
+	}
+	
+	public static void guardarPreferencias() throws IOException {
+		String rutaPerfil = System.getProperty("user.home");
+		File file = new File(rutaPerfil + "\\.BananaTest\\config.properties");
+	
+		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
+			throw new IllegalStateException("No se pudo crear la carpeta .VentanaConMemoria");
+		
+		// Creamos el fichero en caso de no existir
+		file.createNewFile();
+		
+		Preferencias.properties.store(new FileWriter(file), null);
+	}
+	
+	public static TEMAS getTema() {
+		String tema = properties.getProperty("tema");
+		
+		if (tema.toUpperCase().equals(TEMAS.DARK.toString()))
+			return TEMAS.DARK;
+		else
+			return TEMAS.DEFAULT;
+	}
+	
+	public static void setTema(TEMAS tema) {
+		switch (tema) {
+			case DEFAULT:
+				properties.setProperty("tema", "default");
+				break;
+			case DARK:
+				properties.setProperty("tema", "dark");
+				break;
+		}
+	}
+	
+	public static String cargarTema() {
+		return Preferencias.class.getResource("/css/" + properties.getProperty("tema") + ".css").toExternalForm();
 	}
 
 }
