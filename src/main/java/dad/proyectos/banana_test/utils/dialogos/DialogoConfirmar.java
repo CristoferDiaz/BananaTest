44705f8fe5cca7cf10e2c2148c3ce73f6d66b50e
César Ravelo Martínez <crmprograming@gmail.com>
2021-02-15 20:59:23 +0000

    Cambiada la dependencia a la librería fontawesome por Ikonli

--- src/main/java/dad/proyectos/banana_test/utils/dialogos/DialogoConfirmar.java
+++ src/main/java/dad/proyectos/banana_test/utils/dialogos/DialogoConfirmar.java
@@ -1,13 +1,12 @@
 package dad.proyectos.banana_test.utils.dialogos;
 
-import de.jensd.fx.glyphs.GlyphsDude;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 
 public class DialogoConfirmar extends Dialogo<Boolean> {
 
@@ -20,14 +19,15 @@ public class DialogoConfirmar extends Dialogo<Boolean> {
 		
 		Label lbContenido = new Label(contenido);
 		lbContenido.setStyle("-fx-font-weight:bold;");
-		Text warningIcon = GlyphsDude.createIcon(FontAwesomeIcon.WARNING, "120px");
+		FontIcon fontIcon = new FontIcon("fa-exclamation-triangle");
+		fontIcon.setIconSize(120);
 		DropShadow dropShadow = new DropShadow();
 		dropShadow.setOffsetX(3.0f);
 		dropShadow.setColor(Color.color(0.4f, 0.4f, 0.4f));
-		warningIcon.setFill(Color.rgb(232, 178, 36));
-		warningIcon.setEffect(dropShadow);
+		fontIcon.setFill(Color.rgb(232, 178, 36));
+		fontIcon.setEffect(dropShadow);
 		
-		root.getChildren().addAll(lbContenido, warningIcon);
+		root.getChildren().addAll(lbContenido, fontIcon);
 		
 		getDialogPane().setContent(root);
 		
