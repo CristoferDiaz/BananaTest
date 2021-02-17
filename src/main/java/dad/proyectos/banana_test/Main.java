492be6e3bfac49ed021f9952bf9716f89665a1ea
SamirElKharrat <samirelkharrat@gmail.com>
2021-02-17 10:09:51 +0000

    arreglos

--- src/main/java/dad/proyectos/banana_test/Main.java
+++ src/main/java/dad/proyectos/banana_test/Main.java
@@ -1,21 +1,9 @@
 package dad.proyectos.banana_test;
 
-import dad.proyectos.banana_test.db.Intermedio;
-import dad.proyectos.banana_test.model.preguntas.PreguntaTestMultiple;
-import dad.proyectos.banana_test.model.preguntas.PreguntaTestSimple;
-
 public class Main {
 
 	public static void main(String[] args) {
-		String[] respuestas = {"patata", "zanahoria", "legumbre", "lechuga"};
-		boolean[] correctas = {true, false, true, false};
-		String[] error = {"error"};
-		//App.main(args);
-		PreguntaTestSimple simple = new PreguntaTestSimple("Prueba", respuestas);
-		PreguntaTestMultiple multiple = new PreguntaTestMultiple("Prueba", respuestas, correctas);
-		Intermedio.crearPregunta(simple, error);
-		Intermedio.crearPregunta(multiple, error);
-
+		App.main(args);
 	}
 
 }
